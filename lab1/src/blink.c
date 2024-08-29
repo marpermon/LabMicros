07b4de90a76603a3a6fac36a2dc6c7bdf85cabfd
marpermon <maria.perezmonge@ucr.ac.cr>
2024-08-28 19:32:16 -0600

    Intento condensado

--- lab1/src/blink.c
+++ lab1/src/blink.c
@@ -1,53 +1,49 @@
 #include <pic14/pic12f683.h>
-typedef unsigned int word ;
+typedef unsigned int word;
 word __at 0x2007 __CONFIG = _WDTE_OFF & _PWRTE_OFF & _MCLRE_OFF & _CP_OFF & _CPD_OFF & _BOREN_OFF & _IESO_OFF & _FCMEN_OFF;
+#define TIME 0.5
 
+unsigned char lfsr = 0xFF, bit;
+unsigned char actual = 1, nrandom=1, state;
+enum estado {generando, emitiendo};
+unsigned char numbers[10];
  
 void delay (unsigned int tiempo);
+void pulse (unsigned int tiempo);
  
-void main(void)
-{
+void main(void){
 
      TRISIO = 0b00001000; //Poner todos los pines como salidas
 	GPIO = 0x00; //Poner pines en bajo
+	OPTION_REG = 0x07;
 	//WPU = 0b00010000;
 	//NOT_GPPU =0;
+	bit = ((lfsr >> 7) ^ (lfsr >> 5) ^ (lfsr >> 4) ^ (lfsr >> 3)) & 1;
+        lfsr = (lfsr << 1) | bit;
 
-	unsigned short int actual = 0, nrandom = 0;
-	enum estado {generando, emitiendo};
-	unsigned short int state = generando;
-    	float time = 0.1;
-	unsigned int numbers[10];
-	
+        // Scale LFSR value to the range 0 to 99
+        nrandom = (lfsr * 100) >> 8;	
  
-while(1){
-	
-	switch (state){
-		case generando:
-		//la primera vez, si GP3==0 no se hace nada hasta que se active
-			while (GP3 == 1){	//generar numero pseudorandom
-				actual = 0;
-				if (nrandom < 99) nrandom = nrandom + 1;
-				else nrandom = 0; //que no se pase de 99
-			}
-			state = generando;
-		
-		case emitiendo:	
-			while((GP3==0)&&(actual < nrandom))
-				{
-				GP0 = 0x00;
-				delay(time);
-				GP0 = ~GP0;
-				delay(time);
-				actual++;									
-			}
-			if (actual>=nrandom ) state = generando;
+	while(1){	
+		while(GP3==0){
+			pulse(TIME);
+			actual++;
+			if (actual >= nrandom) {
+				while(GP3==0){};
+			}												
 		}
 	}	
-		//generar es para emitir la variable sólo una vezdespués de apretar el botón							
+		//generar es para emitir la variable sólo una vezdespués de apretar el botón
+							
 }
  
-
+void pulse (unsigned int tiempo)
+{
+	GP0 = 0x00;
+	delay(tiempo);
+	GP0 = ~GP0;
+	delay(tiempo);
+}
 
 void delay(unsigned int tiempo)
 {
