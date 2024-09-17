d15c9bbbe586bb90c5fbb3dc2ca155c5216cc75c
marpermon <maria.perezmonge@ucr.ac.cr>
2024-09-17 11:38:40 -0600

    esqueleto rutinas ISR

--- lab2/src/blink.c
+++ lab2/src/blink.c
@@ -1,10 +1,10 @@
 #include <avr/io.h>
+#include <stdbool.h> // para usar bools
 
 #define INICIAL_PARPADEOS 4
 #define INICIAL_PERIODO 2000 // en milisegundos
 
 void parpadear(int cantidad, int periodo);
-bool esBotonEncendido();
 bool esEntradaCorrecta();
 void emitirLuces();
 void esperarEntrada();
@@ -16,38 +16,63 @@ typedef enum {
     ESPERA_ENTRADA      // Espera entrada
 } Estado;
 
+int parpadeos = INICIAL_PARPADEOS;
+int periodo = INICIAL_PERIODO;
+volatile bool esBotonEncendido = 0;
+volatile bool empezamos = 0;
+volatile bool delay = 0;
+int patron[10] = {1,2,4,3,1,1,3,4,2,3} //leds que se encenderán
+int entrada[10];
+
+// Lógica de entradas
+
 ISR(PCINT0_vect){ //subrutina de interrupción con el vector pcint0
+    switch (estado) {
+            case IDLE:
+                if ((PINB7 | PINB6 | PINB5 | PINB4) & (~esBotonEncendido)){
+                    esBotonEncendido = 1;
+                }
+                break;
 
+            case PATRON:
+                break;
 
+            case ESPERA_ENTRADA:
+               
+                break;
 
-
+            default:
+                // Manejar estado desconocido
+                //estado = IDLE;
+                break;
+    }
 }
 
+
+ISR(TIMER0_OVF_vect)
+{
+    // Code for handling Timer 0 Overflow
+}
 
 int main(void)
 {
  DDRB = 0x0F; //Configuracion del puerto como entrada o salida
- GIMSK |= (1 << PCIE0)
- PCMSK0 |= (1 << PCINT7)
- PCMSK0 |= (1 << PCINT6)
- PCMSK0 |= (1 << PCINT5)
- PCMSK0 |= (1 << PCINT4)
- sei()
+ GIMSK |= (1 << PCIE0);
+ PCMSK0 |= (1 << PCINT7);
+ PCMSK0 |= (1 << PCINT6);
+ PCMSK0 |= (1 << PCINT5);
+ PCMSK0 |= (1 << PCINT4);
+ sei();
 
-int parpadeos = INICIAL_PARPADEOS;
-int periodo = INICIAL_PERIODO;
   //Parpadear
   while (1) {
         switch (estado) {
             case IDLE:
-                parpadeos = INICIAL_PARPADEOS;
-                periodo = INICIAL_PERIODO;
-                // Configurar parpadeos iniciales y periodo
-                parpadear(parpadeos, periodo);
-
+            // Configurar parpadeos iniciales y periodo
+                
                 // Verificar botón
-                if (esBotonEncendido()) {
-                    parpadear(2, periodo);
+                if (esBotonEncendido) {
+                    parpadear(parpadeos, periodo);
                     estado = PATRON;
                 }
                 break;
@@ -58,9 +83,7 @@ int periodo = INICIAL_PERIODO;
                 estado = ESPERA_ENTRADA;
                 break;
 
-            case ESPERA_ENTRADA:
-                // Esperar entrada del usuario
-                esperarEntrada();
+            case ESPERA_ENTRADA: // Esperar entrada del usuario
 
                 // Verificar entrada del usuario
                 if (esEntradaCorrecta()) {
@@ -98,11 +121,6 @@ int periodo = INICIAL_PERIODO;
 
 void parpadear(int cantidad, int periodo) {
     // Implementación para parpadear luces LED cantidad de veces con el periodo dado
-}
-
-bool esBotonEncendido() {
-    // Implementación para verificar si el botón está encendido
-    return false; // Placeholder
 }
 
 bool esEntradaCorrecta() {
