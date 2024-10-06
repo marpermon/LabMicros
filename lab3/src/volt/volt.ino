aa85b2d1d0feda332a76d5889f9d915c1d72f2cb
marpermon <maria.perezmonge@ucr.ac.cr>
2024-10-05 18:43:32 -0600

    Valor rms corregido

--- lab3/src/volt/volt.ino
+++ lab3/src/volt/volt.ino
@@ -4,13 +4,13 @@
 #define ANALOG3 A3
 #define ACDC 8
 #define FRECUENCIA 60
-#define SAMPLES 1000
+#define SAMPLES 500
 
 
 #include <PCD8544.h>
-double referencia = 4.84; // tensión máxima de las señales
 double V0, V1, V2, V3;
-double SquareV0 = 0.0, SquareV1 = 0.0, SquareV2 = 0.0, SquareV3 = 0.0;
+double MaxV0 = 0.0, MaxV1 = 0.0, MaxV2 = 0.0, MaxV3 = 0.0;
+double referencia = 4.84; // tensión máxima de las señales
 
 PCD8544 pantalla;
 
@@ -20,8 +20,8 @@ void puerto_serial(double V0, double V1, double V2, double V3); // funcion para 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600); // Puerto serial   
-  analogReference(referencia);
-  pinMode(ACDC, INPUT);
+  analogReference(referencia);// Desingnamos la referencia como la tensión máxima recibida por el arduino, que equivaldrá a 1023
+  pinMode(ACDC, INPUT); // Usamos el interruptor como input
   pantalla.begin();
 }
 
@@ -30,10 +30,10 @@ void loop() {
 
 switch (digitalRead(ACDC)) {
   case HIGH: // En DC
-    V0 = map(analogRead(ANALOG0), 0, 1023,-24,24);   // leer entrada analógica y transformarla al rango original
-    V1 = map(analogRead(ANALOG1), 0, 1023,-24,24); 
-    V2 = map(analogRead(ANALOG2), 0, 1023,-24,24); 
-    V3 = map(analogRead(ANALOG3), 0, 1023,-24,24); 
+    V0 = (analogRead(ANALOG0) * 48.0 / 1023.0) - 24.0;   // leer entrada analógica y transformarla al rango original
+    V1 = (analogRead(ANALOG1) * 48.0 / 1023.0) - 24.0; 
+    V2 = (analogRead(ANALOG2) * 48.0 / 1023.0) - 24.0; 
+    V3 = (analogRead(ANALOG3) * 48.0 / 1023.0) - 24.0; 
     pantalla.setCursor(0, 1); // Coloca lo que se va a imprimir en una posición enl a pantalla
     imprimir_lcd("V0 =", V0); // Imprime en la pantalla
 
@@ -53,37 +53,32 @@ switch (digitalRead(ACDC)) {
 
   case LOW: //En AC
     for (int i = 0; i < SAMPLES; i++) { 
-    /* Usamos 100 samples en un periodo para hacer la una suma de cuadrados
-    aproximada, segun la formula del valor RMS*/
-        V0 = map(analogRead(ANALOG0), 0, 1023,-24,24);   
-        V1 = map(analogRead(ANALOG1), 0, 1023,-24,24); 
-        V2 = map(analogRead(ANALOG2), 0, 1023,-24,24); 
-        V3 = map(analogRead(ANALOG3), 0, 1023,-24,24); 
-        SquareV0 += sq(V0); 
-        SquareV1 += sq(V1); 
-        SquareV2 += sq(V2); 
-        SquareV3 += sq(V3); 
-        delayMicroseconds(16); // 16 us 1000 veces hacen un periodo de 0.16 s, o 62hz
+    /* Usamos 500 samples en un periodo para determinar el
+    valor pico de la señal*/
+        V0 = (analogRead(ANALOG0) * 48.0 / 1023.0) - 24.0;   // leer entrada analógica y transformarla al rango original
+        V1 = (analogRead(ANALOG1) * 48.0 / 1023.0) - 24.0; 
+        V2 = (analogRead(ANALOG2) * 48.0 / 1023.0) - 24.0; 
+        V3 = (analogRead(ANALOG3) * 48.0 / 1023.0) - 24.0;
+        MaxV0 = (V0 > MaxV0) ? V0 : MaxV0;
+        MaxV1 = (V1 > MaxV1) ? V1 : MaxV1;
+        MaxV2 = (V2 > MaxV2) ? V2 : MaxV2;
+        MaxV3 = (V3 > MaxV3) ? V3 : MaxV3; 
     }
 
-    SquareV0 = sqrt(SquareV0/SAMPLES); 
-    SquareV1 = sqrt(SquareV1/SAMPLES); 
-    SquareV2 = sqrt(SquareV2/SAMPLES); 
-    SquareV3 = sqrt(SquareV3/SAMPLES);
 
     pantalla.setCursor(0, 1);
-    imprimir_lcd("V0 RMS=", SquareV0);
+    imprimir_lcd("V0 RMS=", MaxV0/sqrt(2));
 
     pantalla.setCursor(0, 2);
-    imprimir_lcd("V1 RMS=", SquareV1);   
+    imprimir_lcd("V1 RMS=", MaxV1/sqrt(2));   
 
     pantalla.setCursor(0, 3);
-    imprimir_lcd("V2 RMS=", SquareV2);
+    imprimir_lcd("V2 RMS=", MaxV2/sqrt(2));
 
     pantalla.setCursor(0, 4);
-    imprimir_lcd("V3 RMS=", SquareV3);
+    imprimir_lcd("V3 RMS=", MaxV3/sqrt(2));
 
-    puerto_serial(SquareV0, SquareV1, SquareV2, SquareV3);
+    puerto_serial(MaxV0/sqrt(2), MaxV1/sqrt(2), MaxV2/sqrt(2), MaxV3/sqrt(2));
   }
 
   
